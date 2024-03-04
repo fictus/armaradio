62989091dd5f3d2d1b96ab7bd37100e6d50888b4
Luis Valle <luis.e.valle@gmail.com>
2024-03-04 14:31:30 -0600

    initial commit

---
+++ armaradio/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace armaradio.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
