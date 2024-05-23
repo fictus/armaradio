a8f9f4bb2a14588dfb680c6c7e2b0d4ddb5ad75a
Luis Valle <luis.e.valle@gmail.com>
2024-05-23 11:49:44 -0500

    armaminer - added a way to store the sync startup time so it can be easily updated if needed;

--- arma_miner/Worker.cs
+++ arma_miner/Worker.cs
@@ -1,3 +1,4 @@
+using arma_miner.Data;
 using arma_miner.Service;
 
 namespace arma_miner
@@ -6,14 +7,17 @@ namespace arma_miner
     {
         private readonly ILogger<Worker> _logger;
         public readonly IArmaMinerService _armaMinerService;
+        private readonly IDapperHelper _dapper;
 
         public Worker(
             ILogger<Worker> logger,
-            IArmaMinerService armaMinerService
+            IArmaMinerService armaMinerService,
+            IDapperHelper dapper
         )
         {
             _logger = logger;
             _armaMinerService = armaMinerService;
+            _dapper = dapper;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -25,9 +29,10 @@ namespace arma_miner
                     _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
                 }
 
-                // we only want to execute any valid process between 8:00-9:00 PM
-                DateTime minTime = DateTime.Today.AddHours(20);
-                DateTime maxTime = DateTime.Today.AddHours(21);
+                // we only want to execute any valid process between the times included in the settings
+
+                DateTime minTime = _dapper.GetFirstOrDefault<DateTime>("radioconn", "Operations_Sync_GetStartingTime"); //DateTime.Today.AddHours(20);
+                DateTime maxTime = minTime.AddHours(1);
                 DateTime currentTime = DateTime.Now;
 
                 if (currentTime >= minTime && currentTime <= maxTime)
