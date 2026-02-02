219594d147448e2f7cb55ae78862bcf62b2f324c
luis <luis.e.valle@gmail.com>
2026-02-01 21:12:01 -0600

    armaradio - other changes;

--- armaradio/Repositories/ArmaYoutubeDownloader.cs
+++ armaradio/Repositories/ArmaYoutubeDownloader.cs
@@ -89,7 +89,7 @@ namespace armaradio.Repositories
 
                 //options.ExtractorArgs = "youtube:player_client=android_skdless";
                 options.ExtractorArgs = "youtube:player_client=android_music";
-                options.PostprocessorArgs = "--remote-components ejs:github";
+                options.PostprocessorArgs = "'--remote-components', 'ejs:github'";
 
                 //options.ExtractorArgs = "--no-cookies-update"; // "youtube:player_client=android,ios";
                 options.AddHeaders = new MultiValue<string>();
