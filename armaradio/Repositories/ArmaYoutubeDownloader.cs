54ee51bd42e9a2c2599f7b65e6d984a42ccc12c2
luis <luis.e.valle@gmail.com>
2026-02-01 22:11:16 -0600

    armarad - other player;

--- armaradio/Repositories/ArmaYoutubeDownloader.cs
+++ armaradio/Repositories/ArmaYoutubeDownloader.cs
@@ -90,7 +90,7 @@ namespace armaradio.Repositories
                 //};
 
                 //options.ExtractorArgs = "youtube:player_client=android_skdless";
-                options.ExtractorArgs = "youtube:player_client=web";
+                options.ExtractorArgs = "youtube:player_client=android_testsuite";
                 //options.PostprocessorArgs = "--remote-components ejs:github";
 
                 //options.ExtractorArgs = "--no-cookies-update"; // "youtube:player_client=android,ios";
